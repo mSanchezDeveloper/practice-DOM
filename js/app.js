1828364a7b559e9fe5ca46fdedc82466c2021439
Marcelo <marcelo.s44@gmail.com>
2023-01-04 20:21:32 -0500

    DOM practica, ver mas

--- js/app.js
+++ js/app.js
@@ -12,21 +12,31 @@ function cambiarTitulo() {
 }
 
 function verMas() {
-    console.log("Desde ver más");
-    // Create paragraph
-    let newParagraph = document.createElement("p");
-    // Add text
-    newParagraph.innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, voluptatibus eveniet. Reiciendis vel sapiente nisi esse laboriosam ut architecto voluptates aliquam atque quos dolore itaque sequi ratione, fuga maiores similique soluta harum! Aliquam illum, fuga ad nihil iusto sit, facere facilis inventore nisi reprehenderit ratione ea. Ipsum, dolor dolores, incidunt quo iure atque veniam itaque quos tenetur quam laudantium impedit. Aspernatur omnis eum temporibus aperiam ab repellat, labore unde numquam laboriosam ea mollitia minus quae nemo totam accusantium incidunt quod similique iste. Soluta mollitia voluptatem nulla reiciendis est? Sint repudiandae ad est saepe, esse aspernatur? Maxime blanditiis temporibus laudantium praesentium."
-    newParagraph.className = "lead";
-    // Insert in html
-    let articleParents = document.getElementsByTagName("article");
-    articleParents[1].appendChild(newParagraph)
     //Change button
     let btnMore = document.querySelector("#btn-more");
-    btnMore.innerHTML = "Ocultar info";
-    btnMore.className = "btn btn-danger"
+    // Insert in html
+    let articleParents = document.getElementsByTagName("article");
 
-    console.log(newParagraph);
-    console.log(articleParents[1]);
-    console.log(btnMore)
+    if (btnMore.className === "btn btn-primary my-2") {
+        // Create paragraph
+        let newParagraph = document.createElement("p");
+        // Add text
+        newParagraph.innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, voluptatibus eveniet. Reiciendis vel sapiente nisi esse laboriosam ut architecto voluptates aliquam atque quos dolore itaque sequi ratione, fuga maiores similique soluta harum! Aliquam illum, fuga ad nihil iusto sit, facere facilis inventore nisi reprehenderit ratione ea. Ipsum, dolor dolores, incidunt quo iure atque veniam itaque quos tenetur quam laudantium impedit. Aspernatur omnis eum temporibus aperiam ab repellat, labore unde numquam laboriosam ea mollitia minus quae nemo totam accusantium incidunt quod similique iste. Soluta mollitia voluptatem nulla reiciendis est? Sint repudiandae ad est saepe, esse aspernatur? Maxime blanditiis temporibus laudantium praesentium."
+        newParagraph.className = "lead";
+        articleParents[1].appendChild(newParagraph)
+        // Change button text
+        btnMore.innerHTML = "Ocultar info";
+        btnMore.className = "btn btn-danger"
+        console.log(newParagraph)
+    } else {
+        console.log("Delete p")
+        //Delete node
+        console.log(articleParents[1].hasChildNodes());
+        console.log(articleParents[1].children);
+        //Delete p
+        articleParents[1].removeChild(articleParents[1].children[2])
+        btnMore.innerHTML = "Ver más...";
+        btnMore.className = "btn btn-primary my-2"
+    }
+
 } 
